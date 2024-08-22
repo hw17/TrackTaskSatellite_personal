3142c9acf8b36c9bc5617dba51f9f91a3326dba4
Hector Guarneros <48600637+hw17@users.noreply.github.com>
2024-08-23 00:17:05 +0200

    Update client.cpp
    Update and refactoring code:
    -Implement Dynamic allocation of the TCPClient objects to config/init/connect socket and read buffer
    -Refactoring the code by deleting testing code which had the purpose to get familiar with the Server behavior
    -adding threads and delete the testing super loop
    -delete and update comments and unnecessary code

--- client.cpp
+++ client.cpp
@@ -1,175 +1,48 @@
-//if include <json/value.h> line fails (latest kernels), try also:
-//  #include <jsoncpp/json/json.h>
-/*#include <json/value.h>
-#include <fstream>
+#include <iomanip>
+#include <chrono>
+#include <thread>
+#include <mutex>
 
-std::ifstream people_file("people.json", std::ifstream::binary);
-Json::Value people;
-people_file >> people;
-
-cout<<people; //This will print the entire json object.
-
-//The following lines will let you access the indexed objects.
-cout<<people["Anna"]; //Prints the value for "Anna"
-cout<<people["ben"]; //Prints the value for "Ben"
-cout<<people["Anna"]["profession"]; //Prints the value corresponding to "profession" in the json for "Anna"
-
-cout<<people["profession"]; //NULL! There is no element with key "profession". Hence a new empty element will be created.
-*/
-
-#include <iostream>
-#include <cstring>
-#include <cstdlib>
-#include <unistd.h>
-#include <netdb.h>
-#include <sys/socket.h>
-
-#include <vector>
+#include "classClient.h"
 #include "time.h"
-
-//class Client
+#include "mythread.h"
 
 int main(int argc, char *argv[])
 {
-    const char *port = "4001", *port2 = "4002", *port3 = "4003"; // "65001" / 4001
-    char *server = "0.0.0.0";
-    struct addrinfo hints, *host;
-    struct addrinfo hints2, *host2;
-    struct addrinfo hints3, *host3;
-    int r, sockfd;
-    int r2, sockfd2;
-    int r3, sockfd3;
-    // char buffer[BUFSIZ];
-    // char buffer2[BUFSIZ];
-    // char buffer3[BUFSIZ];
-    // std::vector<std::string> buffer(BUFSIZ);
-    // std::vector<std::string> buffer2(BUFSIZ);
-    // std::vector<std::string> buffer3(BUFSIZ);
-    std::vector<std::string> buffer1(BUFSIZ);
-    std::string buffer2;
-    std::string buffer3;
+    char *port1 = "4001", *port2 = "4002", *port3 = "4003"; 
+    char *server = LOCAL_HOST; //"0.0.0.0";
 
-    if (argc < 2)
-    {
-        std::cerr << "Format: client hostname" << std::endl;
-        exit(1);
-    }
-    //server = argv[1];
+    //Create clients
+    //TODO: Need to refactor this sections
+    TCPClient *client1 = new TCPClient();
+    TCPClient *client2 = new TCPClient();
+    TCPClient *client3 = new TCPClient();
 
-    /* obtain and convert server name and port */
-    std::cout << "Looking for server on " << server << "...";
-    memset(&hints, 0, sizeof(hints)); /* use memset_s() */
-    memset(&hints2, 0, sizeof(hints2)); /* use memset_s() */
-    memset(&hints3, 0, sizeof(hints3)); /* use memset_s() */
+    //Configure clients
+    //TODO: Need to refactor this sections
+    client1->configConnection(port1, server);
+    client2->configConnection(port2, server);
+    client3->configConnection(port3, server);
+    client1->connectInit();
+    client2->connectInit();
+    client3->connectInit();
 
-    hints.ai_family = AF_INET;        /* IPv4 */
-    hints.ai_socktype = SOCK_STREAM;  /* TCP */
+    // Threads to handle server sockets
+    std::thread t1(thread_readValues, std::ref(*client1), 1);
+    std::thread t2(thread_readValues, std::ref(*client2), 2);
+    std::thread t3(thread_readValues, std::ref(*client3), 3);
 
-    hints2.ai_family = AF_INET;        /* IPv4 */
-    hints2.ai_socktype = SOCK_STREAM;  /* TCP */
-    
-    hints3.ai_family = AF_INET;        /* IPv4 */
-    hints3.ai_socktype = SOCK_STREAM;  /* TCP */
-    
-    r = getaddrinfo(server, port, &hints, &host);
-    r2 = getaddrinfo(server, port2, &hints2, &host2);
-    r3 = getaddrinfo(server, port3, &hints3, &host3);
-    if ((r != 0) || (r2 != 0) || (r3 != 0))
-    {
-        perror("failed");
-        exit(1);
-    }
-    std::cout << "found" << std::endl;
+    // Thread to printing values
+    std::thread t4(thread_printValues);
 
-    /* create a socket */
-    sockfd = socket(host->ai_family, host->ai_socktype, host->ai_protocol);
-    if (sockfd == -1)
-    {
-        perror("failed");
-        exit(1);
-    }
+    if (t1.joinable()) t1.join();
+    if (t2.joinable()) t2.join();
+    if (t3.joinable()) t3.join();
+    if (t4.joinable()) t4.join();
 
-    sockfd2 = socket(host2->ai_family, host2->ai_socktype, host2->ai_protocol);
-    if (sockfd2 == -1)
-    {
-        perror("failed");
-        exit(1);
-    }
-
-    sockfd3 = socket(host3->ai_family, host3->ai_socktype, host3->ai_protocol);
-    if (sockfd == -1)
-    {
-        perror("failed");
-        exit(1);
-    }
-
-    /* connect to the socket */
-    r = connect(sockfd, host->ai_addr, host->ai_addrlen);
-    if (r == -1)
-    {
-        perror("failed");
-        exit(1);
-    }
-
-    r2 = connect(sockfd2, host2->ai_addr, host2->ai_addrlen);
-    if (r2 == -1)
-    {
-        perror("failed");
-        exit(1);
-    }
-
-    r3 = connect(sockfd3, host3->ai_addr, host3->ai_addrlen);
-    if (r3 == -1)
-    {
-        perror("failed");
-        exit(1);
-    }
-
-    unsigned int miliseconds = 1000;
-    std::string num1,num2,num3;
-    std::ostringstream strOutput;
-
-    /* loop to interact with the server */
-    while (true)
-    {
-        usleep(miliseconds);
-        r = recv(sockfd, buffer1.data(), BUFSIZ, 0);
-        r2 = recv(sockfd2, buffer2.data(), BUFSIZ, 0);
-        r3 = recv(sockfd3, buffer3.data(), BUFSIZ, 0);
-
-        //Create an istringstream object and initialize it with the buffer data
-        std::istringstream iss1(*buffer1.data());
-        std::istringstream iss2(buffer2.data());
-        std::istringstream iss3(buffer3.data());
-        if ((r < 1) || (r2<1) || (r3<1))
-        {
-            puts("Connection closed by peer");
-            break;
-        }
-        /* local input  */
-        //extracts the available value from the stream and assigns it to num
-        iss1 >> num1;
-        iss2 >> num2;
-        iss3 >> num3;
-        double now = std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::system_clock::now().time_since_epoch()).count(); //get EPOCH time
-        strOutput << "{\"timestamp\": " << std::fixed << std::setprecision(0) << now << ", "\
-                  /*<< "\"-out1\": \"" << buffer1.front().substr(0,buffer1.front().find("\n")) <<"\"\t, " \*/
-                  << "\"-out1\": \"" << (num1 == "" ? "--" : num1)  <<"\"\t, " \
-                  << "\"-out2\": \"" << (num2 == "" ? "--" : num2) << "\"\t, " \
-                  << "\"-out3\": \"" << (num3 == "" ? "--" : num3) << "\"} "<< std::endl;
-        std::cout << strOutput.str();
-
-    } /* end while loop */
-
-    /* all done, clean-up */
-    freeaddrinfo(host);
-    freeaddrinfo(host2);
-    freeaddrinfo(host3);
-    close(sockfd);
-    close(sockfd2);
-    close(sockfd3);
-    puts("Disconnected");
-
+    // all done, clean-up
+    delete client1;
+    delete client2;
+    delete client3;
     return 0;
 }
-
